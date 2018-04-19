53afd41a90718271de392febf47950f66fc8c529
victorbofill <victorm.bofill@gmail.com>
2018-04-19 16:46:24 -0700

    basic framework for server and functionality set up

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const app = require('./lib/app');
+const connect = require('./lib/util/connect');
+
+const PORT = 3000;
+
+const MONGODB_URI = 'mongodb://localhost:27017/bananas';
+
+connect(MONGODB_URI);
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log('server running on port ', PORT);
+});
---
+++ test/e2e/db.js
@@ -0,0 +1,15 @@
+require('dotenv').config({ path: './test/e2e/.enc' });
+const connect = require('../../lib/util/connect');
+const mongoose = require('mongoose');
+
+before(() => connect('mongodb://localhost:27019/bananas-test'));
+after(() => mongoose.connection.close());
+
+module.exports = {
+    dropCollection(name) {
+        return mongoose.connection.dropCollection(name)
+            .catch(err => {
+                if(err.codeName !== 'NamespaceNotFound') throw err;
+            }
+    }
+}
