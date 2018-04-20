b2aeee5b2a205e55d10a3b3267e4e142d9860b8c
victorbofill <victorm.bofill@gmail.com>
2018-04-20 10:37:55 -0700

    committing to merge to debug

--- test/e2e/studio.test.js
+++ test/e2e/studio.test.js
@@ -7,5 +7,29 @@ describe('Studio E2E Testing', () => {
     before(() => dropCollection('actors'));
     before(() => dropCollection('studios'));
 
-    let universal = 
+    let universal = {
+        name: 'Universal Studios',
+        state: 'CA',
+        country: 'United States'
+    };
+
+    const checkOk = res => {
+        if(!res.ok) throw res.error;
+        return res;
+    };
+
+    it('saves a studio', () => {
+        return request.post('/studios')
+            .send(universal)
+            .then(checkOk)
+            .then(( {body }) => {
+                const { _id, __v } = body;
+                assert.ok(_id);
+                assert.equal(__v, 0);
+                assert.deepEqual(body, {
+                    _id, __v,
+                    ...universal
+                });
+            });
+    });
 });
