c6ee2f09fa03f524a64a586811c286448fba18aa
Jen Lipton <jkl.lipton@gmail.com>
2018-04-20 09:33:37 -0700

    debug

--- test/e2e/db.js
+++ test/e2e/db.js
@@ -1,8 +1,8 @@
-require('dotenv').config({ path: './test/e2e/.enc' });
+require('dotenv').config({ path: './.env' });
 const connect = require('../../lib/util/connect');
 const mongoose = require('mongoose');
 
-before(() => connect('mongodb://localhost:27019/bananas-test'));
+before(() => connect(process.env.MONGODB_URI));
 after(() => mongoose.connection.close());
 
 module.exports = {
