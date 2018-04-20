d1d334d87e430d15128dcbcc70d445d89532b753
Jen Lipton <jkl.lipton@gmail.com>
2018-04-20 09:20:43 -0700

    pushing

---
+++ test/e2e/actor.test.js
@@ -0,0 +1,42 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
+
+describe('Actor E2E API', () => {
+
+
+    before (() => dropCollection('bannanas'));
+
+    let felicia =  {
+        name: 'Felicity Day',
+        dob: new Date(1979, 6, 28),
+        pob: 'Huntsville, AL'
+    };
+
+    // let someone = {
+    //     stuff
+    // };
+
+    const checkOk = res => {
+        if(!res.ok) throw res.error;
+        return res;
+    };
+
+    it('saves and gets an actor', () => {
+
+        return request.post('/actors')
+            .send(felicia)
+            .then(checkOk)
+            .then(({ body }) => {
+                const { _id, __v } = body;
+                assert.ok( _id );
+                assert.equal(__v, 0);
+                assert.deepEqual(body, {
+                    _id, __v,
+                    ...felicia
+                });
+                felicia = body;
+            });
+    });
+
+});
