ab9c73926e0307b52bec03d173ed137f324433ec
victorbofill <victorm.bofill@gmail.com>
2018-04-20 10:28:01 -0700

    fixed some server errors

--- test/e2e/db.js
+++ test/e2e/db.js
@@ -5,11 +5,11 @@ const mongoose = require('mongoose');
 before(() => connect(process.env.MONGODB_URI));
 after(() => mongoose.connection.close());
 
-// module.exports = {
-//     dropCollection(name) {
-//         return mongoose.connection.dropCollection(name)
-//             .catch(err => {
-//                 if(err.codeName !== 'NamespaceNotFound') throw err;
-//             });
-//     }
-// };
+module.exports = {
+    dropCollection(name) {
+        return mongoose.connection.dropCollection(name)
+            .catch(err => {
+                if(err.codeName !== 'NamespaceNotFound') throw err;
+            });
+    }
+};
