c9cdd05b37cce3288106836cd3782744fe07add9
victorbofill <victorm.bofill@gmail.com>
2018-04-20 10:08:41 -0700

    committing to debug

---
+++ test/e2e/studio.test.js
@@ -0,0 +1,11 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
+const Studio = require('../../lib/models/Studio');
+
+describe('Studio E2E Testing', () => {
+    before(() => dropCollection('actors'));
+    before(() => dropCollection('studios'));
+
+    let universal = 
+});
