0f20898332cb82006bba244cb95f77ab492a6334
victorbofill <victorm.bofill@gmail.com>
2018-04-23 12:00:44 -0700

    studio successfully populated

--- test/e2e/film.test.js
+++ test/e2e/film.test.js
@@ -3,7 +3,7 @@ const request = require('./request');
 const { dropCollection } = require('./db');
 const Film = require('../../lib/models/Film');
 
-describe('Film E2E Testing', () => {
+describe.only('Film E2E Testing', () => {
 
     let studio = {
         name: 'Universal Studios',
@@ -92,8 +92,13 @@ describe('Film E2E Testing', () => {
             .send(trekWars)
             .then(checkOk)
             .then(( { body }) => {
-                const {_id, __v, title, released, studio, cast } = trekWars;
-                assert.deepEqual(body, {_id, __v, title, released, studio, cast });
+                const {_id, __v, title, released, cast } = trekWars;
+                assert.deepEqual(body, {_id, __v, title, released,
+                    studio: {
+                        _id: trekWars.studio,
+                        name: 'Universal Studios'
+                    },
+                    cast });
             });
     });
 
