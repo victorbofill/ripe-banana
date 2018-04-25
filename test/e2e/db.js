9884056e531550f1ee22be0720cdc2bef4f13d40
victorbofill <victorm.bofill@gmail.com>
2018-04-25 13:06:11 -0700

    updated mongo port for travis

--- test/e2e/db.js
+++ test/e2e/db.js
@@ -3,7 +3,7 @@ const connect = require('../../lib/util/connect');
 const mongoose = require('mongoose');
 const request = require('./request');
 
-before(() => connect(process.env.MONGODB_URI || 'mongodb://localhost:27019/pirates-test'));
+before(() => connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pirates-test'));
 after(() => mongoose.connection.close());
 
 module.exports = {
