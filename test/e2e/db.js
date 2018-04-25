4aa92ea98855d465e3f47d68ae6cdf0abd20fd02
victorbofill <victorm.bofill@gmail.com>
2018-04-25 09:42:49 -0700

    db.js updated

--- test/e2e/db.js
+++ test/e2e/db.js
@@ -1,15 +1,24 @@
 require('dotenv').config({ path: './.env' });
 const connect = require('../../lib/util/connect');
 const mongoose = require('mongoose');
+const request = require('./request');
 
-before(() => connect(process.env.MONGODB_URI));
+before(() => connect(process.env.MONGODB_URI || 'mongodb://localhost:27019/pirates-test'));
 after(() => mongoose.connection.close());
 
 module.exports = {
+
     dropCollection(name) {
         return mongoose.connection.dropCollection(name)
             .catch(err => {
                 if(err.codeName !== 'NamespaceNotFound') throw err;
             });
+    },
+
+    createToken(data = { email: 'email@yahoo.com', password: 'equifax' }) {
+        return request
+            .post('./api/auth/signup')
+            .send(data)
+            .then(res => res.body.token);
     }
 };
